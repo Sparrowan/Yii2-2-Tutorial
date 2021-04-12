42c62a2bc5efa05709ae69c5ea953a0226ec9845
Alphius Wambua <alphiuswambua@gmail.com>
2021-04-12 15:02:47 +0300

    Added ajax search

--- backend/views/companies/index.php
+++ backend/views/companies/index.php
@@ -3,6 +3,7 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 use dosamigos\datepicker\DatePicker;
+use yii\widgets\Pjax;
 
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\CompaniesSearch */
@@ -22,6 +23,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 <br/>
 <br/>
+<?php Pjax::begin(); ?>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
@@ -52,6 +54,6 @@ $this->params['breadcrumbs'][] = $this->title;
             ['class' => 'yii\grid\ActionColumn'],
         ],
     ]); ?>
-
+<?php Pjax::end(); ?>
 
 </div>
